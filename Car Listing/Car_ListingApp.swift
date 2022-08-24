8b106c1dfd0b5c8eee4e0f5a3b629ccdff1c1f3a
Tony Dinh <tonydinh442@gmail.com>
2022-08-24 11:53:03 -0500

    Initial Commit

---
+++ Car Listing/Car_ListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_ListingApp.swift
+//  Car Listing
+//
+//  Created by Tony Dinh on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Car_ListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car Listing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Car Listing
+//
+//  Created by Tony Dinh on 8/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
